c58af3cb992e82e43ea547bb781913135d5e188c
Meissa <meissasy@gmail.com>
2024-06-25 22:18:57 -0400

    finish 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
 
-};
+//npm test getTheTitles.spec.js
+const getTheTitles = function(arr) {
+return arr.map((element)=>{
+     return element.title;
+})
+};// ['Book','Book2']
 
 // Do not edit below this line
 module.exports = getTheTitles;
